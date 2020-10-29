f51d48b9ca1aacc0f48ee5a5fc565f314e5f6392
Oliwer <oe222ez@student.lnu.se>
2020-10-29 12:20:30 +0100

    class Player stores total value of cards

--- src/Players.js
+++ src/Players.js
@@ -6,10 +6,13 @@
  */
 
 export class Players {
-    constructor (name) {
+    constructor (name, hand, totVal) {
     this.playerName = name 
     this.hand = {} //spelarens kort 
     this.totVal = 0
+    }
+    set setSum (sum) { //lägger in nya summan (inte än)
+      this.totVal = sum
     }
     toString () {
       return `spelaren heter: ${this.playerName}, spelaren har följande kort: ${this.hand} och summan är: ${this.totVal} `
--- src/app.js
+++ src/app.js
@@ -19,15 +19,39 @@ try {
     // ...shuffle cards 
     //Deck.shuffle(playingCards) //använd sen
 
-  const createPlayer = new Players()
+  const createPlayer = new Players() //skapar EN ny spelare
 
-  createPlayer.playerName = 'Player 1'
+  createPlayer.playerName = 'Player 1' //sätter namnet på spelaren
 
-  createPlayer.hand = playingCards.splice(0, 3)
+  createPlayer.hand = playingCards.splice(0, 3) // ger spelaren tre kort (ska egentligen börja med 1!!)
 
-  console.log(createPlayer.toString())
 
+  //sätter spelarens summa
+  var sum = 0
+  const numOfCards = createPlayer.hand.length - 1
+  for (let i = 0; i <= numOfCards ; i++) {
+    sum = sum + createPlayer.hand[i].rank
+  }
+
+  console.log('summan är ', sum)
+
+
+
+  
+ //visar spelarens kort i en tabell
   console.table(createPlayer.hand)
+
+
+  
+  //settern i player lägger till sum i spelarens totVal
+  createPlayer.setSum = sum
+
+
+  //skriver ut kortens totala summa
+  console.log(createPlayer.totVal)
+
+  //skriver ut en text med spelarens data
+  console.log(createPlayer.toString())
 
 } catch (e) {
   console.error(e.message)
