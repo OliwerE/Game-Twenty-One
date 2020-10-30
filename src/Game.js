2c44392b783f4be733978ffa2e68f3f0f42ded1e
Oliwer <oe222ez@student.lnu.se>
2020-10-30 13:55:42 +0100

    player get second card when round starts

--- src/Game.js
+++ src/Game.js
@@ -15,6 +15,7 @@ export class CreateGame {
     this.players = []
     this.deck = []
     this.deckUsed = []
+    this.numberOfPlayers = 0
   }
   gameSetup () {
     this.deck = Deck.create() //skapar kortleken
@@ -24,36 +25,90 @@ export class CreateGame {
     // return this.deck  //debug, visar hela kortleken
 
     //create players + gives first card
-    const numOfPlayers = 2 //ta bort hårdkodning!
+    this.numberOfPlayers = 1 //ta bort hårdkodning!
+    const numOfPlayers = this.numberOfPlayers
 
-    for (let i = 0; i <= numOfPlayers - 1; i++) { // skapar spelarna (ej dealer)
+    for (let i = 0; i <= numOfPlayers - 1; i++) { // skapar spelarna och summerar (ej dealer)
       
       // creates player
       let createPlayerNumber = i + 1
       let createPlayerName = 'Player #' + createPlayerNumber + ': '
       this.players.push(new CreatePlayer(createPlayerName))
       
-      //gives all players three cards
-      this.players[i].hand = this.deck.splice(0, 3) // ger spelaren tre kort (ska egentligen börja med 1!!)
+      //gives all players first card
+      this.players[i].hand = this.deck.splice(0, 1) // ger spelaren första kortet
       
 
       //sum cards
       let handLenght = this.players[i].hand.length
-      for (let a = 0; a <= handLenght - 1 ; a++) { // Summerar de första korten
+      for (let a = 0; a <= handLenght - 1 ; a++) { // Summerar de första korten (behöver ingen loop för att summera ett kort!!)
         this.players[i].totVal = this.players[i].totVal + this.players[i].hand[a].rank
       }
     }
-
     console.table(this.players) // debug listar spelarna
 
-    // gives all players one card:
+    //console.table(this.players[0]) //spelare 1
+  }
+  gameRound (playerIndex) { //spelar med en spelare
+    var playerId = playerIndex
+ 
+    // ger det andra "startkortet"
+    var tempHand = this.deck.splice(0, 1)
+
+    //visar temphand ska innehålla ett kort
+    //console.table(tempHand)
+    
+    const fixedHand = this.players[0].hand.concat(tempHand)
+    tempHand = []
+    this.players[0].hand = fixedHand
+    
+    console.log('här får spelaren det första (tot andra) kortet för omgången') 
+    console.table(this.players)
+    
+    //visar temphand ska vara tom
+    //console.table(tempHand)
+
+
+    //slut det andra startkortet
 
 
 
   }
-  toString () {
+  /*toString () {
     return `Spelarna i denna omgång är ( ${this.players} ), Kortleken har dessa kort ( ${this.deck} ), de använda korten är ( ${this.deckUsed} ).`
+  }*/
+  debugPlayers () {
+    return this.players
+  }
+  /*debugCardsLeft () {
+    return this.deck
+  }
+  debugUsedCards () {
+    return this.deckUsed
+  }*/
+  startGame () {
+    //setup
+    this.gameSetup()
+
+    //Dealer play with players one at a time
+    const numOfPlayers = this.numberOfPlayers
+    for (let i = 0; i <= numOfPlayers - 1; i++) {
+      this.gameRound(i)
+    }
+    
   }
 }
+
+
+/* lägger till kort från player index i slänghögen
+this.deckUsed = this.players[0].hand.splice(0, 1)
+*/
+
+/*
+// test skapa metod utanför utanför en klass
+CreateGame.prototype.testMethod = function() {
+  return 'method outside class'
+}
+*/
 
 //Begin Game
--- src/app.js
+++ src/app.js
@@ -15,38 +15,16 @@ import { CreatePlayer } from './CreatePlayer.js'
 import { CreateGame } from './Game.js'
 
 try {
-  /*
-  const createPlayer = new CreatePlayer() //skapar EN ny spelare
-
-  createPlayer.playerName = 'Player #1:' //sätter namnet på spelaren
-
-  createPlayer.hand = playingCards.splice(0, 3) // ger spelaren tre kort (ska egentligen börja med 1!!)
-
-
-  //sätter spelarens summa
-  var sum = 0
-  const numOfCards = createPlayer.hand.length - 1
-  for (let i = 0; i <= numOfCards ; i++) {
-    sum = sum + createPlayer.hand[i].rank
-  }
-
-  //visar spelarens kort i en tabell
-  //console.table(createPlayer.hand)
-
-
-  
-  //settern i player lägger till sum i spelarens totVal
-  createPlayer.setSum = sum
-
-  //skriver ut en text med spelarens data
-  console.log(createPlayer.toString())
-  */
   const theGame = new CreateGame()
-  theGame.gameSetup()
-  console.log(theGame.toString())
+  theGame.startGame()
+  //console.log(theGame.toString())
 
 
-
+  //debug
+  console.log('---------------------------')
+  console.log(`${theGame.debugPlayers()}`)
+  //console.log(theGame.debugCardsLeft())
+  //console.log(theGame.debugUsedCards())
 
 } catch (e) {
   console.error(e.message)
